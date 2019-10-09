cdd51284001ccee4c0d9e70593f41417001e735f
Douglas Beal <public.git@dougbeal.com>
2019-10-08 18:13:30 -0700

    checkout hugo componenet modues into $HOME/sites/

--- checkout.sh
+++ checkout.sh
@@ -1,10 +1,15 @@
-#!/bin/bas
+#!/bin/bash
 IFS=$'\n'
+base="${HOME}/git/sites"
+mkdir -p "${base}" &&
 (
+
     for repl in $(grep replace go.mod); do
-        repo=${repl##replace }
-        repo=${repo%% =>*}
-        cd ~/git
-        git clone "https://${repo}"
+        (
+            repo=${repl##replace }
+            repo=${repo%% =>*}
+            cd "${base}"
+            git clone "https://${repo}"
+        )
     done
 )
