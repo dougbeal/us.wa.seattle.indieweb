5c454bfc02cc1c2ef50e11fab1de2cfa86ddb38c
Douglas Beal <public.git@dougbeal.com>
2019-10-10 13:48:12 -0700

    ignore generated files

--- status.sh
+++ status.sh
@@ -3,14 +3,14 @@ IFS=$'\n'
 base="${HOME}/git/sites"
 [ -e "${base}" ] &&
 (
-
+    git status -s
     for repl in $(grep replace go.mod); do
         (
             repo=${repl##*replace*github*/*/}
             repo=${repo%% =>*}
             cd "${base}/${repo}"
             echo "${repo}"
-            git commit -am "$@"
+            git status -s
         )
     done
 )
