d0a516d5b1491e137ba5c5a4e98b4806a5ea9168
Douglas Beal <public.git@dougbeal.com>
2019-10-18 19:56:38 -0700

    New author, Oct 30 hwc
    - more useful event archetypes/event
    - add stevestreza author
    - create oct 30
    - add Cody to author.yml

--- push.sh
+++ push.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 . github.sh
-do_site_git push --tags
+do_site_git push --follow-tags -u origin master
+do_site_git push --tags -u origin master
--- status.sh
+++ status.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 . github.sh
 do_site_git status -s
+do_site_git tag --list
 
